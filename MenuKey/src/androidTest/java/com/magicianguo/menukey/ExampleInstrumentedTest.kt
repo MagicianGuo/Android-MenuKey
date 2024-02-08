b39292c07d8a2ed9d24aea04d9ad66ebc2c12d9b
guojc <2639880697@qq.com>
2024-02-08 17:10:43 +0800

    Fix package

--- MenuKey/src/androidTest/java/com/magicianguo/menukey/ExampleInstrumentedTest.kt
+++ MenuKey/src/androidTest/java/com/magicianguo/menukey/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.gjc.menukey
+package com.magicianguo.menukey
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.gjc.menukey", appContext.packageName)
+        assertEquals("com.magicianguo.menukey", appContext.packageName)
     }
 }
